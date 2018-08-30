a55cfc8c4bf9034b444b4dc1224796d7b327ef66
Ernest Johanes <ernestjohanes@Ernests-MacBook-Pro.local>
2018-08-30 16:33:55 +0700

    tugas

---
+++ Week_2/Berlatih Perulangan Javascript/ganjil-genap.js
@@ -0,0 +1,51 @@
+// Ganjil - Genap
+
+var counter = 1
+while (counter <= 100) {
+  if (counter % 2 == 0) {
+    console.log(counter + " genap")
+    } else {
+    console.log(counter + " ganjil")  
+  }
+  counter = counter + 1
+}
+console.log(counter + " lebih dari 100")
+
+// Pertambahan 2 dengan kelipanan 3
+
+var counter = 1
+while (counter <= 100) {
+  if (counter % 3 == 0) {
+    console.log(counter + " Kelipatan 3")
+    } else {
+    console.log(counter + "")  
+  }
+  counter = counter + 2
+}
+console.log(counter + " lebih dari 100")
+
+// Pertambahan 5 dengan kelipatn 6
+
+var counter = 1
+while (counter <= 100) {
+  if (counter % 6 == 0) {
+    console.log(counter + " Kelipatan 6")
+    } else {
+    console.log(counter + "")  
+  }
+  counter = counter + 5
+}
+console.log(counter + " lebih dari 100")
+
+// Pertambahan 9 dengan kelipatan 10
+
+var counter = 1
+while (counter <= 100) {
+  if (counter % 10 == 0) {
+    console.log(counter + " Kelipatan 10")
+    } else {
+    console.log(counter + "")  
+  }
+  counter = counter + 9
+}
+console.log(counter + " lebih dari 100")
---
+++ Week_2/Mengenal Penggunaan Function/shoutout.js
@@ -0,0 +1,15 @@
+// Opsi 1
+
+function shoutOut(){
+  console.log('Halo Function!');
+}
+  
+shoutOut();
+
+// Opsi 2
+
+function shoutOut(){
+  return "Halo Function!"
+}
+
+console.log(shoutOut())
