e4b287a4f919ee3186512ed34a5737fdaca048cb
fender13 <ej.fender@outlook.com>
2018-11-15 16:28:59 +0700

    Remove duplicated directory

--- week_2/Berlatih Perulangan Javascript/loop_for.js
+++
@@ -1,7 +0,0 @@
-console.log("LOOPING PERTAMA")
-for (var looping1 = 0; looping1 <= 20; looping1++) {
-  console.log(looping1 + " - I love coding")
-} console.log("LOOPING KEDUA")
-  for (var looping2 = 20; looping2 > 0; looping2--) {
-  console.log(looping2 + " - I will become fullstack developer")
-}
