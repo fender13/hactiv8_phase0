410fa198ee2a924dfbf5931f5bd5931165cc2979
fender13 <ej.fender@outlook.com>
2018-12-04 10:00:18 +0700

    Revised

--- week2/2_pseudocode.js
+++ week2/2_pseudocode.js
@@ -1,6 +1,4 @@
-// Newton
-
-  // natural lang
+  // Natural Lang Newton
 
 // Jika diketahui :
 //   F = m * A 
@@ -8,99 +6,68 @@
 //           A = 2
 //     Hitung gaya yang dihasilkan?
 
-  // Algoritma
+  // Pseudocode Newton
 
-// newtons = mass * scalar
-// store 'mass' with 600
-// store 'scalar with 2
-// store 'newtons' with value
-// calculate newtons
+// store 'mass' with any number (600)
+// store 'scalar with any number (2)
+// store 'newtons' with 0
+// calculate newtons 
+    // mass * Scalar
 // set 'newtons' with calculation result
 
-var mass = 600;
-var scalar = 2;
-var newtons = 0;
-newtons = mass * scalar
-console.log(newtons);
+===============================
 
-// Kabisat
-
-  // Natural Lang
+  // Natural Lang Kabisat
 
 // diketahui bahwa tanggal 29 februari merupakan tahun kabisat:
 // kriteria tahun kabisat:
 //   tahun habis di bagi 4 dan tidak habis di bagi 100
 //   tahun habis di bagi 4, habis di bagi 100 dan habis di bagi 400
 
-  // Algoritma
+  // Pseudocode Kabisat
 
-// store year (number)
-// store kabisat
-  // if kabisat = year % 4 == 0 && year % 100 != 0
-  // if kabisat = year % 4 == 0 && year % 100 = 0 && year % 400 = 0
-  // else ("Bukan Kabisat")
+// store year with any year number
+// store kabisat with string
+  // jika kabisat merupakan year yang habis di bagi 4 dan year yang tidak habis di bagi 100
+  // dan jika kabisat merupakan year yang habis di bagi 4 dan year yang habis di bagi 100 dan year yang habis di bagi 400
+  // di luar itu bukan kabisat
 
-var year = 2000;
-var kabisat;
-if ((year % 4 == 0) && (year % 100 != 0)) {
-  year = ("Kabisat");
-} else if ((year % 4 == 0) && (year % 100 == 0)  && (year % 400 == 0)) {
-  year = ("Kabisat");
-} else {
-  year = ("Bukan Kabisat");
-}
+===============================
 
-// Laundry
-
-  // Natural Lang
+  // Natural Lang Laundry
 
 // Diketahui akan cuci 20 baju
 //   cuci tidak akan mulai jika baju belum masuk semua ke mesin
 
-  // Algortima
+  // Pseudocode Laundry
 
-// store cloth with 0
-  // if cloth <= 20
-    // display laundy not start
-    // add cloth with 1
-// end 
-// display laundry start
+// store baju with 0
+//  selama jumlah baju yang masuk kurang dari 20 baju 
+      // masukan baju satu per satu (baju = baju + 1)
+    // jika total baju sama dengan pas 20 baju
+    // end while
+    // display mesin cuci siap 
 
-var cloths = 0
-while (cloths < 20) {
-  console.log ("Laundry Not Start");
-  cloths++
-} 
-console.log ("LaundryStart");
+===============================
 
-// Kuku
-
-  // Natural Lang
+  // Natural Lang Kuku
 
 // Diketahui total siswa 40
 // Guru keliling cek kuku kuku_murid
 //   Jika ketemu kuku panjang = dihukum
 //   Jika tidak panjang = dipuji
 
-  // Algoritma
+  // Pseudocode Kuku
 
-// store siswa = 40
-// while
-  // if kuku panjang = hukuman
-  // if kuku pendek = pujian
+// store nilai kuku panjang = 0
+// store nilai kuku pendek = 0
+
+// selama jumlah siswa yang diperiksa belum mencapai 40
+  // jika selama di perika ketemu kuku panjang
+    // nilai kuku panjang bertambah 1
+  // jika selama di perika ketemu kuku pendek
+    // nilai kuku pendek bertambah 1
+  //jika seluruh siswa sudah diperiksa kukunya
 // end while
-// display siswa
+// display jumlah siswa yang terkena hukuman dan pujian
 
-
-var text = "";
-var i = 0;
-var kuku_murid = ['panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang','panjang','pendek','pendek','panjang'];
-while (i <= 40) {
-    text += "<br>Siswa/siswi ke " + i +" kukunya = "+ kuku_murid[i];
-    if (kuku_murid [i] == 'panjang'){
-      text += " => Hukum";
-    } else{
-      text += " => Puji";
-  }
-  i++;
-}
